8eefe33db393414fb5fc66abf118eb1ce38fe690
mn46 <m.nowak033@gmail.com>
2022-06-02 17:43:42 +0200

    fetched data and made template work

---
+++ script/product-list.js
@@ -0,0 +1,34 @@
+// fetching the data - books
+
+let books_url = 'http://mnowak.dk/wp-mindspace/wp-json/wp/v2/book?per_page=100'
+
+fetch(books_url)
+    .then(function(res) {
+        return res.json();
+    })
+    
+    .then(function(data) {
+        dataReceived(data);
+})
+
+
+function dataReceived(data) {
+    data.forEach(showBook);
+}
+
+function showBook(books) {
+
+    const bookTemplate = document.querySelector("#book-card-template").content;
+
+    bookClone = bookTemplate.cloneNode(true);
+
+    // adding data
+
+    bookClone.querySelector("img.book-image").src = books.book_picture;
+
+    bookClone.querySelector(".book-genre").textContent = books.categories;
+
+
+    document.querySelector(".all-products").appendChild(bookClone);
+
+}
